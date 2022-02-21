d1313bd276349cfc3fe54bc55ae03bba548dd39b
Simcha Schaum <simcha.schaum@gmail.com>
2022-02-21 12:02:48 +0200

    contact page done

--- src/pages/lomdei/Contact.js
+++ src/pages/lomdei/Contact.js
@@ -1,31 +1,46 @@
 import TopNavbar from "../../topNavbar/TopNavbar";
 import Footer from "../../footer/Footer";
+import { db } from "../../firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 const Contact = () => {
 
+    const [contentArr, setContentArr] = useState([]);
+
+    const getData = async () => {
+      const docRef = doc(db,"website-info","contact-us-intro");
+      const snapshot = await getDoc(docRef);
+      if(snapshot.exists){
+        setContentArr([...snapshot.data().content]);
+        console.log(contentArr)
+      } else {
+        console.log("Sorry! No data available.");
+      }
+    }
+
+    useEffect(()=>{getData()},[]);
+    useEffect(()=>console.log(contentArr),[contentArr])
     return(
         <div>
-            <div class="pageContainer">
+            <div className="pageContainer">
                 <TopNavbar />
-            <section id="contactusHeader" class="header">
+            <section id="contactusHeader" className="header">
                 <h2>Contact Us</h2> 
             </section>
             <section id="contact-us">
-                <div class="intro-text">
+                <div className="intro-text">
                     <h4>
-                    Lomdei was created with one purpose in mind: להגדיל תורה ולהאדירה. 
-                    We are privileged to partner with so many schools and teachers and are humbled that we are making a 
-                    difference in the limud Torah of thousands of students.
+                    {contentArr.length > 0 ? contentArr[0].text : "Data loading... "}
                     </h4>
                     <h4>
-                    May we be <em>zocheh</em> to continue providing the necessary training, tools, and support to enhance the Limudei Kodesh 
-                    experience of every school, teacher and student.
+                    {contentArr.length > 0 ? contentArr[1].text : "Data loading... "}
                     </h4>
                     <h4>
                     Please contact us to learn more about the ways Lomdei can help your students. 
                     </h4>
                 </div>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8XHmSJSSKx-b19lbVLORg6s9UHuUoU9k5Z-kqGZXwlwOjdw/viewform?embedded=true" title="ContactForm" width="600" height="725" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8XHmSJSSKx-b19lbVLORg6s9UHuUoU9k5Z-kqGZXwlwOjdw/viewform?embedded=true" title="ContactForm" width="600" height="725" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </section>
 
 
